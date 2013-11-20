6164e8a95bc96400b658729a31d2bff67df450da
moteus <mimir@newmail.ru>
2013-11-20 16:55:01 +0400

    Add. HMAC

--- test/test_sha.lua
+++ test/test_sha.lua
@@ -86,25 +86,109 @@ local SHA = {
   -- },
 }
 
+-- rfc4231 and rfc2202
+local HMAC = {
+  { key = HEX"0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b",
+    msg = HEX"4869205468657265", count = 1;
+    {
+      ["SHA-1"  ] = "b617318655057264e28bc0b6fb378c8ef146be00",
+      ["SHA-224"] = "896fb1128abbdf196832107cd49df33f47b4b1169912ba4f53684b22",
+      ["SHA-256"] = "b0344c61d8db38535ca8afceaf0bf12b881dc200c9833da726e9376c2e32cff7",
+      ["SHA-384"] = "afd03944d84895626b0825f4ab46907f15f9dadbe4101ec682aa034c7cebc59cfaea9ea9076ede7f4af152e8b2fa9cb6",
+      ["SHA-512"] = "87aa7cdea5ef619d4ff0b4241a1d6cb02379f4e2ce4ec2787ad0b30545e17cdedaa833b7d6b8a702038b274eaea3f4e4be9d914eeb61f1702e696c203a126854",
+    },
+  },
+  { key = HEX"4a656665",
+    msg = HEX"7768617420646f2079612077616e7420666f72206e6f7468696e673f", count = 1;
+    {
+      ["SHA-1"  ] = "effcdf6ae5eb2fa2d27416d5f184df9c259a7c79",
+      ["SHA-224"] = "a30e01098bc6dbbf45690f3a7e9e6d0f8bbea2a39e6148008fd05e44",
+      ["SHA-256"] = "5bdcc146bf60754e6a042426089575c75a003f089d2739839dec58b964ec3843",
+      ["SHA-384"] = "af45d2e376484031617f78d2b58a6b1b9c7ef464f5a01b47e42ec3736322445e8e2240ca5e69e2c78b3239ecfab21649",
+      ["SHA-512"] = "164b7a7bfcf819e2e395fbe73b56e0a387bd64222e831fd610270cd7ea2505549758bf75c05a994a6d034f65f8f0e6fdcaeab1a34d4a6b4b636e070a38bce737",
+    },
+  },
+  { key = HEX"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    msg = HEX"dd", count = 50;
+    {
+      ["SHA-1"  ] = "125d7342b9ac11cd91a39af48aa17b4f63f175d3",
+      ["SHA-224"] = "7fb3cb3588c6c1f6ffa9694d7d6ad2649365b0c1f65d69d1ec8333ea",
+      ["SHA-256"] = "773ea91e36800e46854db8ebd09181a72959098b3ef8c122d9635514ced565fe",
+      ["SHA-384"] = "88062608d3e6ad8a0aa2ace014c8a86f0aa635d947ac9febe83ef4e55966144b2a5ab39dc13814b94e3ab6e101a34f27",
+      ["SHA-512"] = "fa73b0089d56a284efb0f0756c890be9b1b5dbdd8ee81a3655f83e33b2279d39bf3e848279a722c806b485a47e67c807b946a337bee8942674278859e13292fb",
+    },
+  },
+  { key = HEX"0102030405060708090a0b0c0d0e0f10111213141516171819",
+    msg = HEX"cd", count = 50;
+    {
+      ["SHA-1"  ] = "4c9007f4026250c6bc8414f9bf50c86c2d7235da",
+      ["SHA-224"] = "6c11506874013cac6a2abc1bb382627cec6a90d86efc012de7afec5a",
+      ["SHA-256"] = "82558a389a443c0ea4cc819899f2083a85f0faa3e578f8077a2e3ff46729665b",
+      ["SHA-384"] = "3e8a69b7783c25851933ab6290af6ca77a9981480850009cc5577c6e1f573b4e6801dd23c4a7d679ccf8a386c674cffb",
+      ["SHA-512"] = "b0ba465637458c6990e5a8c5f61d4af7e576d97ff94b872de76f8050361ee3dba91ca5c11aa25eb4d679275cc5788063a5f19741120c4f2de2adebeb10a298dd",
+    },
+  },
+  { key = HEX"0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c",
+    msg = HEX"546573742057697468205472756e636174696f6e", count = 1;
+    {
+      ["SHA-1"  ] = "4c1a03424b55e07fe7f27be1",
+      ["SHA-224"] = "0e2aea68a90c8d37c988bcdb9fca6fa8",
+      ["SHA-256"] = "a3b6167473100ee06e0c796c2955552b",
+      ["SHA-384"] = "3abf34c3503b2a23a46efc619baef897",
+      ["SHA-512"] = "415fad6271580a531d4179bc891d87a6",
+    },
+  },
+  { key = (HEX"aa"):rep(80),
+    msg = "Test Using Larger Than Block-Size Key - Hash Key First", count = 1;
+    {
+      ["SHA-1"  ] = "aa4ae5e15272d00e95705637ce8a3b55ed402112",
+    },
+  },
+  { key = (HEX"aa"):rep(131),
+    msg = "Test Using Larger Than Block-Size Key - Hash Key First", count = 1;
+    {
+      ["SHA-224"] = "95e9a0db962095adaebe9b2d6f0dbce2d499f112f2d2b7273fa6870e",
+      ["SHA-256"] = "60e431591ee0b67f0d8a26aacbf5b77f8e0bc6213728c5140546040f0ee37f54",
+      ["SHA-384"] = "4ece084485813e9088d2c63a041bc5b44f9ef1012a2b588f3cd11f05033ac4c60c2ef6ab4030fe8296248df163f44952",
+      ["SHA-512"] = "80b24263c7c1a3ebb71493c1dd7be8b49b46d1f41b4aeec1121b013783f8f3526b56d037e05f2598bd0fd2215d6a1e5295e64f73f63f0aec8b915a985d786598",
+    },
+  },
+  { key = (HEX"aa"):rep(80),
+    msg = "Test Using Larger Than Block-Size Key and Larger Than One Block-Size Data", count = 1;
+    {
+      ["SHA-1"  ] = "e8e99d0f45237d786d6bbaa7965c7808bbff1a91",
+    },
+  },
+  { key = (HEX"aa"):rep(131),
+    msg = "This is a test using a larger than block-size key and a larger than block-size data. The key needs to be hashed before being used by the HMAC algorithm.", count = 1;
+    {
+      ["SHA-224"] = "3a854166ac5d9f023f54d517d0b39dbd946770db9c2b95c9f6f565d1",
+      ["SHA-256"] = "9b09ffa71b942fcb27635fbcd5b0e944bfdc63644f0713938a7f51535c3a35e2",
+      ["SHA-384"] = "6617178e941f020d351e2f254e8fd32c602420feb0b8fb9adccebb82461e99c5a678cc31e799176d3860e6110c46523e",
+      ["SHA-512"] = "e37b6a775dc87dbaa4dfa9f96e5e3ffddebd71f8867289865df5a32d20cdc944b6022cac3c4982b10d5eeb55c3e4de15134676fb6de0446065c97440fa8c6a58",
+    },
+  },
+}
+
 local FN = {
-  ["SHA-1"  ] = {new = sha.sha1.new,   digest = sha.sha1.digest     };
-  ["SHA-224"] = {new = sha.sha224.new, digest = sha.sha224.digest   };
-  ["SHA-256"] = {new = sha.sha256.new, digest = sha.sha256.digest   };
-  ["SHA-384"] = {new = sha.sha384.new, digest = sha.sha384.digest   };
-  ["SHA-512"] = {new = sha.sha512.new, digest = sha.sha512.digest   };
+  ["SHA-1"  ] = {new = sha.sha1.new,   digest = sha.sha1.digest,   hmac = sha.sha1.hmac,    };
+  ["SHA-224"] = {new = sha.sha224.new, digest = sha.sha224.digest, hmac = sha.sha224.hmac,  };
+  ["SHA-256"] = {new = sha.sha256.new, digest = sha.sha256.digest, hmac = sha.sha256.hmac,  };
+  ["SHA-384"] = {new = sha.sha384.new, digest = sha.sha384.digest, hmac = sha.sha384.hmac,  };
+  ["SHA-512"] = {new = sha.sha512.new, digest = sha.sha512.digest, hmac = sha.sha512.hmac,  };
 }
 
 for _, test in ipairs(SHA) do
   local msg    = assert(test.msg);
   local count  = test.count;
 
- io.write("msessage size " .. (#msg * count) .. ": \n")
+  io.write("msessage size " .. (#msg * count) .. ": \n")
   for algo, etalon in pairs(test[1]) do
     local digest = FN[algo].digest
     local new    = FN[algo].new
     if digest then 
       local hash 
-      io.write("  " .. algo .. " with msessage size " .. (#msg * count) .. " - ")
+      io.write("  " .. algo .. " - ")
   
       if count == 1 then -- do not try allocate big msg
         hash = digest(msg, true) assert(hash == etalon     )
@@ -123,6 +207,43 @@ for _, test in ipairs(SHA) do
         for i = 1, #msg do d:update((msg:sub(i,i))) end
         hash = d:digest(true) assert(hash == etalon     )
         hash = d:digest()     assert(hash == HEX(etalon))
+      end
+      d:destroy()
+      io.write(" pass!\n")
+    end
+  end
+end
+
+for _, test in ipairs(HMAC) do
+  local key    = assert(test.key);
+  local msg    = assert(test.msg);
+  local count  = test.count;
+
+  io.write("msessage size " .. (#msg * count) .. " key size " .. #key .. " : \n")
+  for algo, etalon in pairs(test[1]) do
+    local digest = FN[algo].hmac.digest
+    local new    = FN[algo].hmac.new
+    if digest then 
+      local hash 
+      io.write("  HMAC-" .. algo .. " - ")
+  
+      if count == 1 then -- do not try allocate big msg
+        hash = digest(key, msg, true) assert(hash:sub(1, #etalon)      == etalon     )
+        hash = digest(key, msg)       assert(hash:sub(1, #HEX(etalon)) == HEX(etalon))
+      end
+
+      local d = new(key)
+      for i = 1, count do
+        d:update(msg)
+      end
+      hash = d:digest(true) assert(hash:sub(1, #etalon)      == etalon     )
+      hash = d:digest()     assert(hash:sub(1, #HEX(etalon)) == HEX(etalon))
+
+      if count == 1 then
+        d:reset(key)
+        for i = 1, #msg do d:update((msg:sub(i,i))) end
+        hash = d:digest(true) assert(hash:sub(1, #etalon)      == etalon     )
+        hash = d:digest()     assert(hash:sub(1, #HEX(etalon)) == HEX(etalon))
       end
       d:destroy()
       io.write(" pass!\n")
